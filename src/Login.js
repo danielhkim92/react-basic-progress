a789b97d92efa70dbc517f33fafaa4fb6447ad2e
danielhkim92 <danielhkim92@gmail.com>
2017-11-29 14:54:24 -0600

    login page

---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+
+	login = (e) => {
+		if(e.key === 'Enter'){
+			this.props.login(e.target.value)
+		}
+		console.log(e.target.value)
+	}
+	render() {
+		return (
+			<div>
+				<h5>Login</h5>
+				<input type="text" onKeyPress={this.login}/>
+			</div>
+		)
+	}
+}
+export default Login;
