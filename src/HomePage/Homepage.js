01247cbe9b5f4dfaee971aa5f5a8da944114fa73
danielhkim92 <danielhkim92@gmail.com>
2017-11-30 14:22:03 -0600

    cleaning code

--- src/HomePage/Homepage.js
+++ src/HomePage/Homepage.js
@@ -8,58 +8,55 @@ class Homepage extends Component {
 	render() {
 		return (
 			<div className='row'>
-		
 				<h1>Home Page</h1>
-				<ol>
-				<li>  
-						<div className="switch">
-    						<label>Not Completed
-    							<input type="checkbox"/>
+					<ol>
+						<li>  
+							<div className="switch">
+    							<label>Not Completed
+    								<input type="checkbox"/>
     								<span className="lever"></span>
-    						Completed</label>
-  						</div>	
-
-
-				      <div className="row">
-        <div className="col s12 m6">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">The First Step</span>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div className="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
-          </div>
-        </div>
-      </div>
-			</li>	
-				<li>  
-						<div className="switch">
-    						<label>Not Completed
+    								Completed</label>
+  							</div>	
+						<div className="row">
+        					<div className="col s12 m6">
+          						<div className="card blue-grey darken-1">
+            						<div className="card-content white-text">
+              							<span className="card-title">The First Step</span>
+              								<p>I am a very simple card. I am good at containing small bits of information.
+              								I am convenient because I require little markup to use effectively.</p>
+            						</div>
+            							<div className="card-action">
+              								<a href="#">This is a link</a>
+              								<a href="#">This is a link</a>
+            							</div>
+          							</div>
+        					</div>
+      					</div>
+						</li>	
+						<li>  
+							<div className="switch">
+    							<label>Not Completed
     							<input type="checkbox"/>
-    								<span className="lever"></span>
-    						Completed</label>
-  						</div>	
-
-
-				      <div className="row">
-        <div className="col s12 m6">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">The Second Step</span>
-              <p>WHAT DO I NEED TO DO??????.</p>
-            </div>
-            <div className="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
-          </div>
-        </div>
-      </div>
-			</li>
+    							<span className="lever"></span>
+    							Completed</label>
+  							</div>	
+  							<div className="row">
+        						<div className="col s12 m6">
+          							<div className="card blue-grey darken-1">
+            							<div className="card-content white-text">
+							            	<span className="card-title">The Second Step</span>
+							            		<p>WHAT DO I NEED TO DO??????.</p>
+            							</div>
+            							<div className="card-action">
+							            	<a href="#">This is a link</a>
+							            	<a href="#">This is a link</a>
+            							</div>
+          							</div>
+        						</div>
+      						</div>
+						</li>
+					</ol>
+			</div>
 		)
 	}
 }
