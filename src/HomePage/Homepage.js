fc3b703860c494b355037cbbb02a4246f94b33f6
danielhkim92 <danielhkim92@gmail.com>
2017-12-01 15:57:23 -0600

    playing around with cards

--- src/HomePage/Homepage.js
+++ src/HomePage/Homepage.js
@@ -14,6 +14,23 @@ class Homepage extends Component {
 
 				<h1>Home Page</h1>
 				<MakeGoal/>
+				  <div className="card">
+    <div className="card-content">
+      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+    </div>
+    <div className="card-tabs">
+      <ul className="tabs tabs-fixed-width">
+        <li className="tab"><a href="#test4">Test 1</a></li>
+        <li className="tab"><a class="active" href="#test5">Test 2</a></li>
+        <li className="tab"><a href="#test6">Test 3</a></li>
+      </ul>
+    </div>
+    <div className="card-content grey lighten-4">
+      <div id="test4">Test 1</div>
+      <div id="test5">Test 2</div>
+      <div id="test6">Test 3</div>
+    </div>
+  </div>
 					<ol>
 						<li>  
 							<div className="switch">
