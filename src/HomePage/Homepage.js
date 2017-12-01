5a60af0cf3af403782c99436f76d07ced1a2cad0
danielhkim92 <danielhkim92@gmail.com>
2017-12-01 16:14:10 -0600

    gotta figure out how to do strike through

--- src/HomePage/Homepage.js
+++ src/HomePage/Homepage.js
@@ -40,7 +40,7 @@ class Homepage extends Component {
           						<div className="card blue-grey darken-1">
             						<div className="card-content white-text">
               							<span className="card-title">The First Step</span>
-              								<p className="strikethrough">I am a very simple card. I am good at containing small bits of information.
+              								<p className="line-through">I am a very simple card. I am good at containing small bits of information.
               								I am convenient because I require little markup to use effectively.</p>
             						</div>
             							<div className="card-action">
