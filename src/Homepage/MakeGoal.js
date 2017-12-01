e123fef2d45c0a07b05d71f8e12ff6c188a49b33
danielhkim92 <danielhkim92@gmail.com>
2017-12-01 15:50:49 -0600

    working with the form;

--- src/Homepage/MakeGoal.js
+++ src/Homepage/MakeGoal.js
@@ -10,15 +10,18 @@ class MakeGoal extends Component {
     }
   }
 	handleSubmit = (e) => {
-		console.log('hello')
+		state =  this.state;
+		state.goalTitle = e.currentTarget.title;
+		this.setState(state);
+		console.log(state);
 	}
 	render() {
 		return (
 			<div>
 				<h3>Create a Goal</h3>
 				<form>
-					<label>Goal Title</label><input type='text' value="goalTitle"></input>
-					<label>Summary of Goal</label><input type='text' value="goalSummary"></input>
+					<label>Goal Title</label><input type='text' name="title"></input>
+					<label>Summary of Goal</label><input type='text' name="summary"></input>
 					<button onClick={this.handleSubmit} className="waves-effect waves-light btn">Create Goal</button>
 				</form>
 			</div>
