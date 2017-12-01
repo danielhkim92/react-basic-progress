d5d8e7c708549d1134cc84d07a64362060aa9f33
danielhkim92 <danielhkim92@gmail.com>
2017-12-01 16:02:14 -0600

    cleaned up code

--- src/HomePage/Homepage.js
+++ src/HomePage/Homepage.js
@@ -11,22 +11,21 @@ class Homepage extends Component {
 		return (
 
 			<div className='row'>
-
 				<h1>Home Page</h1>
 				<MakeGoal/>
 				  <div className="card">
-    <div className="card-content">
-      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-    </div>
-    <div className="card-tabs">
-      <ul className="tabs tabs-fixed-width">
-        <li className="tab"><a href="#test4">Test 1</a></li>
-        <li className="tab"><a class="active" href="#test5">Test 2</a></li>
-        <li className="tab"><a href="#test6">Test 3</a></li>
-      </ul>
-    </div>
-    <div className="card-content grey lighten-4">
-      <div id="test4">					 
+				    <div className="card-content">
+				      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+				    </div>
+				    <div className="card-tabs">
+				      <ul className="tabs tabs-fixed-width">
+				        <li className="tab"><a href="#test4">Test 1</a></li>
+				        <li className="tab"><a class="active" href="#test5">Test 2</a></li>
+				        <li className="tab"><a href="#test6">Test 3</a></li>
+				      </ul>
+				    </div>
+				    <div className="card-content grey lighten-4">
+				    	<div id="test4">					 
 							<div className="switch">
     							<label>Not Completed
     								<input type="checkbox"/>
@@ -49,12 +48,12 @@ class Homepage extends Component {
         					</div>
       					</div>
 						</div>
-      <div id="test5"><li>  
+     				 	<div id="test5">
 							<div className="switch">
     							<label>Not Completed
-    							<input type="checkbox"/>
-    							<span className="lever"></span>
-    							Completed</label>
+	    							<input type="checkbox"/>
+	    							<span className="lever"></span>
+	    							Completed</label>
   							</div>	
   							<div className="row">
         						<div className="col s12 m6">
@@ -70,8 +69,8 @@ class Homepage extends Component {
           							</div>
         						</div>
       						</div>
-						</li></div>
-      <div id="test6">Test 3</div>
+						</div>
+      <div id="test6">Ideally we should add more stuff</div>
     </div>
   </div>
 				
