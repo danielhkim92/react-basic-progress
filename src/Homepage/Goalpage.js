52b9b4d74b1b099363a2b1edd40b920d5dcc8358
danielhkim92 <danielhkim92@gmail.com>
2017-12-04 16:16:15 -0600

    this will not work

---
+++ src/Homepage/Goalpage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Goalpage extends Component {
+	constructor(){
+		super();
+		this.state = {
+
+		}
+	}
+	render(){
+
+		return(
+			<div>
+				<h1>OMGOMGOMGOMGOMGOMG</h1>
+			</div>
+			)
+	}
+}
+
+export default Goalpage;
