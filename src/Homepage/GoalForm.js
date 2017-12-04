8913fee98f4dc215c2cc637fd8bc6b8d78403318
danielhkim92 <danielhkim92@gmail.com>
2017-12-04 14:48:31 -0600

    adding in the first step

--- src/Homepage/GoalForm.js
+++ src/Homepage/GoalForm.js
@@ -4,15 +4,28 @@ class GoalForm extends Component {
 	constructor(){
 		super();
 		this.state  = {
-
+			step: "",
+			description: "",
 		}
+	}
+	handleStep = (e) = {
+		this.setState(this.state.step: event.target.value)
+
+	}
+
+	handleDescription = (e) = {
+		this.setState(this.state.description: event.target.value)
+	}
+
+	handleSubmit = (e) => {
+		console.log('the first step has been submitted')
 	}
 	render(){
 		return(
 			<div>
 				<form onSubmit={this.handleSubmit}>
 					<label>Make the first step!:</label>
-					<input type='text' placeholder="Enroll into General Assembly"/>
+					<input type='text' onChange={this.handleStep} placeholder="Enroll into General Assembly"/>
 					<label>Description of first step!:</label>
 					<input type ='text' placeholder="Figure out how can I afford this??"/>
 					<input type='submit' value='Submit'/>
